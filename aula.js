2b00d594577e833340a3606d22bf5fc7ea4e2631
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-02-25 21:36:15 -0300

    Revisados tipos de declarações, tipos de variáveis e operadores de comparação

---
+++ aula.js
@@ -0,0 +1,39 @@
+// var linguagem = "Javascript";
+// console.log("Aprendendo, " + linguagem);
+
+// var linguagem = "Java";
+// console.log("Aprendendo, " + linguagem);
+
+var idade = 18;
+
+console.log("Oi, " + nome);
+
+if (idade >= 18) {
+    var nome = "João";
+    console.log(`Parabéns, ${nome}. Você pode dirigir.`);
+}
+
+console.log(`Até mais, ${nome}`);
+
+console.log(0 == "0");
+console.log(0 == []);
+console.log("0" == []);
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+// começando...
+
+// const nome = "José";
+// const idade = 27;
+// const a;
+
+// let a = 2;
+// let b = "abc";
+// let endereco = "Rua K, 12";
+
+// var c = 2 + 3;
+// var d = "abcd";
