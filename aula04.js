14ce8b999ca934282c504f98b74a81b2f32315e5
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-03-04 19:32:55 -0300

    adicionado exemplos de métodos de array

--- aula04.js
+++ aula04.js
@@ -1,6 +1,13 @@
 const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"];
 const iniciais = nomes.map(nome => nome.charAt(0));
 const comprimentos = nomes.map(nome => nome.length);
+const todosComecamComA = nomes.every(nome => nome.toLocaleLowerCase().startsWith("a"));
 
-console.log(iniciais);
-console.log(comprimentos);
+console.log({ variavel: 'iniciais', data: iniciais });
+console.log({ variavel: 'comprimentos', data: comprimentos });
+console.log({ variavel: 'todosComecamComA', data: todosComecamComA });
+
+const valores = [1, 2, 3, 4];
+const soma = valores.reduce((acumulado, valor)=> acumulado + valor);
+
+console.log({ variavel: 'soma', data: soma });
